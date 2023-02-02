788586267fa5528a910e100b947d16ea9cb37a73
Taamannae Taabassum <Taamannae@users.noreply.github.com>
2023-02-02 15:21:52 -0500

    Cleaning up and adding comments

--- battleship.js
+++ battleship.js
@@ -1,27 +1,24 @@
-const prompt = require('prompt-sync')();
+import promptSync from 'prompt-sync';
+import { prettifyBoard, 
+        print,
+        spotValidator,
+        spotParser,
+        generateBoard } from './helpers.js';
+
+const prompt = promptSync()
 
 var GAME_STATE = {
     stage: 0,
     gameEnded: false
 }
 
-messages = {
+const messages = {
     welcome: "Hello, choose a thing",
     badSpot: 'Uh oh'
 }
 
-const LETTERS = "ABCDEFGHIJ";
 
-const SHIPS = [
-    {name: "Carrier", shipSize: 5},
-    {name: "Battleship", shipSize: 4},
-    {name: "Cruiser", shipSize: 3},
-    {name: "Submarine", shipSize: 3},
-    {name: "Destroyer", shipSize: 2},
-]
-
-
-boards = {
+var boards = {
     player: {
         fullBoard: '',
         viewBoard: [
@@ -68,208 +65,84 @@ boards = {
     ],
 }
 
-function printBoard(board) {
-    console.log('   A B C D E F G H I J')
-    for (let i = 0; i < board.length; i++) {
-        if (i != 9) {
-            console.log(i + 1, '', board[i].join().replaceAll("0", "_").replaceAll("1", "O"))
-        } else {
-            console.log(i + 1, board[i].join().replaceAll("0", "_").replaceAll("1", "O"))
-        }
-    }
-    return ''
-}
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-function spotValidator(spot) {
-    if (spot.length > 3) {
-        return false
-    }
-    let row = spot[0];
-    let col = parseInt(spot.slice(1));
-    if (!LETTERS.includes(row.toUpperCase()) || isNaN(col) || col > 10) {
-        return false
-    }
-    return true;
-}
-
-function spotParser(spot) {
-    let row = spot[0];
-    let col = parseInt(spot.slice(1));
-    return [parseInt(row, 36) - 10, col - 1]
-}
-
-function generateBoard() {
-    board = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
-    shipCoords = [];
-    for (let ship = 0; ship < SHIPS.length; ship++) {
-        shipSize = SHIPS[ship].shipSize
-        shipComplete = false;
-
-        while (!shipComplete) {
-        // Coordinate for the start of the ship 
-        start = [getRandomInt(9), getRandomInt(9)];
-        
-        // Which way to face
-        // UP=0, RIGHT=1, DOWN=2, LEFT=3
-        direction = getRandomInt(4);
-
-        // Make sure ship fits on the board
-        if (direction == 0) {
-            end = [start[0] - shipSize + 1, start[1]];
-        } else if (direction == 1) {
-            end = (start[0], start[1] + shipSize - 1)
-        } else if (direction == 2) {
-            end = (start[0] + shipSize - 1, start[1])
-        } else if (direction == 3) {
-            end = [start[0], start[1] - shipSize + 1]
-        }
-
-        if (!((end[0] >= 0 && end[0] <= 9) && (end[1]>= 0 && end[1]<= 9))) {
-            continue;
-        }
-        
-        UDMin = Math.min(start[0], end[0]);
-        UDMax = Math.max(start[0], end[0]);
-        RLMin = Math.min(start[1], end[1]);
-        RLMax = Math.max(start[1], end[1]);
-        // Check collision and add to board
-        if (direction == 0) { // up
-            addedNum = 0;
-            for (let i = UDMin; i <= UDMax + 1; i++) {
-                addedNum = addedNum + board[i][RLMax]
-            }
-            if (addedNum > 0) {
-                continue
-            } else {
-                for (let i = UDMin; i <= UDMax; i++) {
-                    board[i][RLMax] = 1
-                }
-            }
-        } else if (direction == 1) { // right
-            addedNum = 0;
-            for (let i = RLMin; i <= RLMax + 1; i++) {
-                addedNum = addedNum + board[UDMax][i]
-            }
-            if (addedNum > 0) {
-                continue
-            } else {
-                for (let i = RLMin; i <= RLMax; i++) {
-                    board[UDMax][i] = 1
-                }
-            }
-        }  else if (direction == 2) { // right
-            addedNum = 0;
-            for (let i = UDMin; i <= UDMax + 1; i++) {
-                addedNum = addedNum + board[i][RLMax]
-            }
-            if (addedNum > 0) {
-                continue
-            } else {
-                for (let i = UDMin; i <= UDMax; i++) {
-                    board[i][RLMax] = 1
-                }
-            }
-        } else if (direction == 3) { // right
-            addedNum = 0;
-            for (let i = RLMin; i <= RLMax + 1; i++) {
-                addedNum = addedNum + board[UDMax][i]
-            }
-            if (addedNum > 0) {
-                continue
-            } else {
-                for (let i = RLMin; i <= RLMax; i++) {
-                    board[UDMax][i] = 1
-                }
-            }
-        }
-        shipCoords.push([start, end, SHIPS[ship]])
-        shipComplete = true;
-        }
-    }
-    return [board, shipCoords];
-}
-
 function winCondition() {
     return false
 }
 
-let potentialBoards = [generateBoard(), generateBoard(), generateBoard()]
-
-
-function play() {
-
+function computerSetup() {
     let computerBoard = generateBoard()
     boards.computer.fullBoard = computerBoard[0];
     boards.computer.shipLocation = computerBoard[1];
+    return ''
+}
 
-    console.log(boards.computer.shipLocation);
+function chooseBoard() {
+    let potentialBoards = [generateBoard(), generateBoard(), generateBoard()]
 
+    print('1) Option 1')
+    print(prettifyBoard(potentialBoards[0][0]))
+    print('2) Option 2')
+    print(prettifyBoard(potentialBoards[1][0]))
+    print('3) Option 3')
+    print(prettifyBoard(potentialBoards[2][0]))
+    var boardNum = prompt('Select one of the above boards to play on:');
+
+    while (isNaN(boardNum) || boardNum < 0 || boardNum > 3) {
+        boardNum = prompt('Sorry, not valid. Try again:  ');
+        boardNum = parseInt(boardNum)
+    }
+
+    boards.player.fullBoard = potentialBoards[parseInt(boardNum) - 1][0]
+    boards.player.shipLocation = potentialBoards[parseInt(boardNum) - 1][1]
+}
+
+function playerTurn() {
+    print(prettifyBoard(boards.computer.viewBoard))
+    var spot = prompt('Where do you want to place the hit?');
+    let valid = spotValidator(spot)
+
+    while (!valid) {
+        spot = prompt('Sorry, it must be alpha-numeric like "B9". Please try again: ');
+        valid = spotValidator(spot)
+
+    }
+    spot = spotParser(spot)
+    checkHit(spot, 'PLAYER')
+}
+
+function checkHit(spot, type) {
+    if (type == 'PLAYER') {
+        if (boards.computer.fullBoard[spot[0]][spot[1]] == 1) {
+            boards.computer.viewBoard[spot[0]][spot[1]] = "X"
+            print("Nice! You got a hit")
+            print(prettifyBoard(boards.computer.viewBoard))
+        } else {
+            boards.computer.viewBoard[spot[0]][spot[1]] = "-"
+            print("Boo! You missed")
+            print(prettifyBoard(boards.computer.viewBoard))
+        }
+        GAME_STATE.stage += 1;
+    } else {
+        //computer move
+    }
+}
+
+function play() {
     while (!GAME_STATE.gameEnded) {
         switch (GAME_STATE.stage) {
             case 0: //Welcome State
-                console.log(messages.welcome)
-                console.log('1) Option 1')
-                console.log(printBoard(potentialBoards[0][0]))
-                console.log('2) Option 2')
-                console.log(printBoard(potentialBoards[1][0]))
-                console.log('3) Option 3')
-                console.log(printBoard(potentialBoards[2][0]))
-                
-                var boardNum = prompt('Select one of the above boards to play on:');
-
-                while ( isNaN(boardNum) || boardNum < 0 || boardNum > 3) {
-                    boardNum = prompt('Sorry, not valid. Try again:  ');
-                    boardNum = parseInt(boardNum)
-                }
-
-                boards.player.fullBoard = potentialBoards[parseInt(boardNum) - 1][0]
-                boards.player.shipLocation = potentialBoards[parseInt(boardNum) - 1][1]
+                print(messages.welcome)
+                computerSetup()
+                chooseBoard()
+                print("Let's start the game")
                 GAME_STATE.stage += 1;
                 break;
             case 1: // Player Turn
-                console.log("Let's start the game")
-                console.log("Here is your game view \n")
-                console.log(printBoard(boards.computer.viewBoard))
-                var spot = prompt('Where do you want to place the hit?');
-                let valid = spotValidator(spot)
-
-                while (!valid) {
-                    spot = prompt('Sorry, it must be alpha-numeric like "B9". Please try again: ');
-                    valid = spotValidator(spot)
-
-                }
-                spot = spotParser(spot)
-                boards.computer.viewBoard[spot[1]][spot[0]] = "X"
-                if (boards.computer.fullBoard[spot[1]][spot[0]] == 1) {
-                    console.log("Nice! You got a hit")
-                    console.log(printBoard(boards.computer.viewBoard))
-                    boards.computer.fullBoard[spot[1]][spot[0]] = "X"
-                    GAME_STATE.stage += 1;
-
-                } else {
-                    console.log("Boo! You missed")
-                    console.log(printBoard(boards.computer.viewBoard))
-                    GAME_STATE.stage += 1;
-                }
-        
+                print("Here is your game view \n")
+                playerTurn()
                 break;
             case 2: // Computer Turn
-                console.log('Now its the computers turn')
+                print('Now its the computers turn')
                 GAME_STATE.gameEnded = true
                 break;
 
---
+++ helpers.js
@@ -0,0 +1,261 @@
+const SHIPS = [
+    { name: "Carrier", shipSize: 5 },
+    { name: "Battleship", shipSize: 4 },
+    { name: "Cruiser", shipSize: 3 },
+    { name: "Submarine", shipSize: 3 },
+    { name: "Destroyer", shipSize: 2 },
+]
+
+export function print(message) {
+    console.log(message)
+    return ''
+}
+
+export function prettifyBoard(board) {
+    /*
+    Input: An array of arrays
+    This function will take a board (an array of arrays)
+    and print out the stylized, prettified version of that board. The intention is so the player has a cleaner game view
+
+    Example:
+
+    Input: 
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    Output:    
+           A B C D E F G H I J
+        1  _,_,_,_,_,_,_,_,_,_
+        2  O,O,O,O,O,_,_,_,_,_
+        3  _,_,_,O,_,_,_,_,_,_
+        4  _,_,_,O,_,_,_,_,_,_
+        5  _,_,_,O,_,_,_,_,_,_
+        6  _,_,_,_,_,O,O,O,_,_
+        7  _,_,_,_,_,O,_,_,O,_
+        8  _,_,_,_,_,O,_,_,O,_
+        9  _,_,_,_,_,O,_,_,O,_
+        10 _,_,_,_,_,_,_,_,_,_
+    */
+
+    // Print the column headers
+    console.log('   A B C D E F G H I J')
+
+    //loop through the board
+    for (let i = 0; i < board.length; i++) {
+        //If we're not on row 10, add a space because it's 2 digits 
+        if (i != 9) {
+            //print the row but replace the 0s (empty) with '_' and 1s (boat location) with 'O'
+            console.log(i + 1, '', board[i].join().replaceAll("0", "_").replaceAll("1", "O"))
+        } else {
+            console.log(i + 1, board[i].join().replaceAll("0", "_").replaceAll("1", "O"))
+        }
+    }
+    return ''
+}
+
+export function getRandomInt(max) {
+    /*
+    Input: Integer 
+    Returns a random integer between 0 and the number provided
+    */
+    return Math.floor(Math.random() * max);
+}
+
+export function spotValidator(spot) {
+    /*
+    Input: String
+
+    Returns true if the spot the player wants to place the hit is valid on the board. Capitalization does not matter
+
+    Examples
+
+    Input: A12
+    Output: false
+
+    Input: 123
+    Output: False
+
+    Input: C7
+    Output: true
+
+    Input: b10
+    Output: true
+    */
+
+    //Acceptable columns
+    const LETTERS = "ABCDEFGHIJ";
+
+    //The longest spot will be 3 chars (e.g A10)
+    if (spot.length > 3) {
+        return false
+    }
+    //Parsing the row and column out
+    let col = spot[0]; // The letter
+    let row = parseInt(spot.slice(1)); // The number
+    //Checks that Col is a letter from A-J and col is a number less than 11
+    if (!LETTERS.includes(col.toUpperCase()) || isNaN(row) || row > 10) {
+        return false
+    }
+    return true;
+}
+
+
+export function spotParser(spot) {
+    /*
+    Input: string
+
+    Return a machine readable array of the spot in [row, col] format
+
+    Input: A10
+    Output: [9,0]
+
+    Input: C6
+    Output: [5,2]
+
+    */
+    let col = spot[0];
+    let row = parseInt(spot.slice(1));
+    return [row - 1, parseInt(col, 36) - 10]
+}
+
+export function generateBoard() {
+    /*
+    Input: none
+    Returns an array of the board and the coordinate location of the ships representing a playable battleship board
+
+    Output: 
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ],
+        [
+            [ [4, 8], [4, 4],  name: 'Carrier', shipSize: 5 }],
+            [ [1, 5], [1, 2], { name: 'Battleship', shipSize: 4 } ],
+            [ [5, 2], [5, 0], { name: 'Cruiser', shipSize: 3 } ],
+            [ [3, 7], [3, 5], { name: 'Submarine', shipSize: 3 } ],
+            [ [2, 0], [1, 0], { name: 'Destroyer', shipSize: 2 } ]
+        ]
+    */
+
+    //base board
+    var board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    //tracking where the ships start and
+    var shipCoords = [];
+    for (let ship = 0; ship < SHIPS.length; ship++) {
+        var shipSize = SHIPS[ship].shipSize
+        var shipComplete = false;
+
+        while (!shipComplete) {
+            // Coordinate for the start of the ship 
+            const start = [getRandomInt(9), getRandomInt(9)];
+
+            // Which way to face
+            // UP=0, RIGHT=1, DOWN=2, LEFT=3
+            const direction = getRandomInt(4);
+            var end;
+
+            // Make sure ship fits on the board
+            if (direction == 0) {
+                end = [start[0] - shipSize + 1, start[1]];
+            } else if (direction == 1) {
+                end = (start[0], start[1] + shipSize - 1)
+            } else if (direction == 2) {
+                end = (start[0] + shipSize - 1, start[1])
+            } else if (direction == 3) {
+                end = [start[0], start[1] - shipSize + 1]
+            }
+
+            if (!((end[0] >= 0 && end[0] <= 9) && (end[1] >= 0 && end[1] <= 9))) {
+                continue;
+            }
+
+            const UDMin = Math.min(start[0], end[0]);
+            const UDMax = Math.max(start[0], end[0]);
+            const RLMin = Math.min(start[1], end[1]);
+            const RLMax = Math.max(start[1], end[1]);
+            // Check collision and add to board
+            if (direction == 0) { // up
+                var addedNum = 0;
+                for (let i = UDMin; i <= UDMax + 1; i++) {
+                    addedNum = addedNum + board[i][RLMax]
+                }
+                if (addedNum > 0) {
+                    continue
+                } else {
+                    for (let i = UDMin; i <= UDMax; i++) {
+                        board[i][RLMax] = 1
+                    }
+                }
+            } else if (direction == 1) { // right
+                var addedNum = 0;
+                for (let i = RLMin; i <= RLMax + 1; i++) {
+                    addedNum = addedNum + board[UDMax][i]
+                }
+                if (addedNum > 0) {
+                    continue
+                } else {
+                    for (let i = RLMin; i <= RLMax; i++) {
+                        board[UDMax][i] = 1
+                    }
+                }
+            } else if (direction == 2) { // right
+                var addedNum = 0;
+                for (let i = UDMin; i <= UDMax + 1; i++) {
+                    addedNum = addedNum + board[i][RLMax]
+                }
+                if (addedNum > 0) {
+                    continue
+                } else {
+                    for (let i = UDMin; i <= UDMax; i++) {
+                        board[i][RLMax] = 1
+                    }
+                }
+            } else if (direction == 3) { // right
+                addedNum = 0;
+                for (let i = RLMin; i <= RLMax + 1; i++) {
+                    addedNum = addedNum + board[UDMax][i]
+                }
+                if (addedNum > 0) {
+                    continue
+                } else {
+                    for (let i = RLMin; i <= RLMax; i++) {
+                        board[UDMax][i] = 1
+                    }
+                }
+            }
+            shipCoords.push([start, end, SHIPS[ship]])
+            shipComplete = true;
+        }
+    }
+    return [board, shipCoords];
+}
