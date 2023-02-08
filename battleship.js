a10a07d8bfc9907736f92c411eddd5401d314310
Taamannae Taabassum <Taamannae@users.noreply.github.com>
2023-02-08 13:16:34 -0500

    Horizontal printing

--- battleship.js
+++ battleship.js
@@ -65,16 +65,6 @@ const messages = {
 
 `,
     optionText: "First, you must select a game board. Enter '1', '2', or '3'. \nThis will represent the placement of your ships that the computer will try to hit \n",
-    optionNumber: function(num) {
-        return `
-###################
-#                 #
-#     Option ${num}    #
-#                 #
-###################
-`
-
-    },
     boardNum:'Select one of the above boards (1,2,3) to play on:  ',
     badSpot: 'Uh oh'
 }
@@ -131,15 +121,40 @@ function computerSetup() {
     return ''
 }
 
+function printBoardHorizontal(board1, board2, board3) {
+    console.log(Chalk.bgMagenta.black(`
+
+   ###################       ###################       ###################
+   #     Option 1    #       #     Option 2    #       #     Option 3    #
+   ###################       ###################       ###################
+`))
+
+    console.log('   A B C D E F G H I J       A B C D E F G H I J       A B C D E F G H I J')
+
+    //loop through the board
+    for (let i = 0; i < board1.length; i++) {
+        //If we're not on row 10, add a space because it's 2 digits 
+        if (i != 9) {
+            //print the row but replace the 0s (empty) with '_' and 1s (boat location) with 'O'
+            console.log(i + 1, '', 
+                board1[i].join().replaceAll("0", "_").replaceAll("1", "O"), '  ', 
+                i + 1, '', 
+                board2[i].join().replaceAll("0", "_").replaceAll("1", "O"), '  ', 
+                i + 1, '', 
+                board3[i].join().replaceAll("0", "_").replaceAll("1", "O"))
+        } else {
+            console.log(i + 1, board1[i].join().replaceAll("0", "_").replaceAll("1", "O"), '  ', i + 1, board2[i].join().replaceAll("0", "_").replaceAll("1", "O"), '  ', i + 1, board3[i].join().replaceAll("0", "_").replaceAll("1", "O"),)
+        }
+    }
+    console.log('\n\n')
+
+
+}
+
 function chooseBoard() {
     let potentialBoards = [generateBoard(), generateBoard(), generateBoard()]
 
-    print(Chalk.bgMagenta.black(messages.optionNumber(1)))
-    printBoard(potentialBoards[0][0])
-    print(Chalk.bgMagenta.black(messages.optionNumber(2)))
-    printBoard(potentialBoards[1][0])
-    print(Chalk.bgMagenta.black(messages.optionNumber(3)))
-    printBoard(potentialBoards[2][0])
+    printBoardHorizontal(potentialBoards[0][0], potentialBoards[1][0], potentialBoards[2][0])
 
     var boardNum = prompt(messages.boardNum);
 
@@ -200,8 +215,7 @@ function findShip(player, spot) {
                 continue
             }
         }
-    
-        }
+    }
 }
 function checkHit(spot, type) {
     if (type == 'PLAYER') {
